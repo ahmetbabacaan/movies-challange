7ef4dded246a4a72b467cb170eac7cca6abe4965
Ahmet Babacan <ahmet.babacan@yahoo.com>
2022-01-21 21:32:49 +0300

    add first main activity and architecture

---
+++ app/src/main/java/com/ahmetbabacan/movies/ui/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.ahmetbabacan.movies.ui
+
+import com.ahmetbabacan.movies.R
+import com.ahmetbabacan.movies.databinding.ActivityMainBinding
+import com.skydoves.bindables.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main)
